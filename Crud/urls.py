4fd087b09dfc44afb61b88578925fb0b78525900
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-02-19 10:58:17 -0300

    SEGUNDO COMMIT

--- Crud/urls.py
+++ Crud/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('formEditPaciente', views.formEditPaciente, name='formEditPaciente'),
     path('formRegistroPaciente', views.formRegistroPaciente, name='formRegistroPaciente'),
-    
-
+    path('paciente', views.paciente, name='paciente'),
+    path('borrarPaciente/<int:dni>', views.borrarPaciente, name='borrarPaciente'),
 ]
--- Crud/views.py
+++ Crud/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Paciente
+from .forms import PacienteForm
 # Create your views here.
 
 def inicio(request):
     return render(request, 'paginas/paciente/index.html')
 
+def paciente(request):
+    pacientes= Paciente.objects.all()
+    return render(request, 'paginas/paciente/listaPaciente.html',{'pacientes':pacientes})
+
 def formRegistroPaciente(request):
-    return render(request, 'paginas/paciente/formRegistroPaciente.html')
+    formulario= PacienteForm(request.POST or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('paciente')
+    return render(request, 'paginas/paciente/formRegistroPaciente.html',{'formulario':formulario})
 
 def formEditPaciente(request):
     return render(request, 'paginas/paciente/formEditPaciente.html')
 
+def borrarPaciente(request, dni):
+    paciente=Paciente.objects.get(dni=dni)
+    paciente.delete()
+    return redirect('paciente')
